34ae5de26af95d609d139afbf2adf3b2615db08d
Mykola <seletskyi.mykola@gmail.com>
2021-09-04 18:09:59 +0300

    add models

---
+++ Forum/Data/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Nullable<DateTime> EditDate { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
---
+++ Forum/Data/Models/Topic.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class Topic
+    {
+        public int Id {  get; set; }
+        public string Theme {  get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IdentityUser IdentityUser {  get; set;}
+        public HashSet<Post> Posts {  get; set; }
+    }
+}
